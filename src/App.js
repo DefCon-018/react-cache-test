ec7aedb8c39cc6fa60acc23b4586f958b4e0c819
Himanshu Pandey <hp761570@gmail.com>
2022-04-12 15:11:04 +0530

    Add: react-cache

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from "./logo.svg";
-import "./App.css";
+import * as React from "react";
 
-function App() {
+import { useClearCache } from "react-clear-cache";
+
+const App = () => {
+  const { isLatestVersion, emptyCacheStorage } = useClearCache();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello world</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React cache</h1>
+      {!isLatestVersion && (
+        <p>
+          <a
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              emptyCacheStorage();
+            }}
+          >
+            Update version
+          </a>
+        </p>
+      )}
     </div>
   );
-}
+};
 
 export default App;
